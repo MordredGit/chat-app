79bc6df80d1f8dbf88648862f9678cef01637ce9
MordredGit <mshagun2001@gmail.com>
2023-09-09 12:37:15 +0530

    patch: boxshadow bugfix

--- src/components/StarredMessages.tsx
+++ src/components/StarredMessages.tsx
@@ -15,7 +15,7 @@ const StarredMessages = () => {
         {/* Header */}
         <Box
           sx={{
-            boxShadow: "0 0 2 rgba(0,0,0,0.25)",
+            boxShadow: "0 0 2px rgba(0,0,0,0.25)",
             width: "100%",
             bgcolor: theme.palette.background.paper,
           }}
