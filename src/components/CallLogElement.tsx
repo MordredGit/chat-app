a435e4e6a644f5dcbde7eae70a6d5d26b0c74660
MordredGit <mshagun2001@gmail.com>
2023-09-11 23:48:46 +0530

    feat: Add Call Page and its comps

---
+++ src/components/CallLogElement.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Avatar, Box, IconButton, Stack, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import StyledBadge from "./StyledBadge";
+import { CallList } from "../data";
+import { ArrowDownLeft, ArrowUpRight, Phone } from "phosphor-react";
+
+type CallType = (typeof CallList)[0];
+
+const CallLogElement = ({
+  img,
+  name,
+  online,
+  incoming,
+  missed,
+  time,
+}: CallType) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: 75,
+        borderRadius: 1,
+        backgroundColor: theme.palette.background.default,
+      }}
+      p={2}
+    >
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Stack direction={"row"} spacing={2}>
+          {online ? (
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            >
+              <Avatar src={img} alt="user avatar" />
+            </StyledBadge>
+          ) : (
+            <Avatar src={img} alt="user avatar" />
+          )}
+          <Stack spacing={0.3}>
+            <Typography variant="subtitle2">{name}</Typography>
+            <Stack direction={"row"} alignItems={"center"} spacing={1}>
+              {incoming ? (
+                <ArrowDownLeft color={missed ? "red" : "green"} />
+              ) : (
+                <ArrowUpRight color={missed ? "red" : "green"} />
+              )}
+              <Typography variant="caption">
+                {"Yesterday, "}
+                <Typography variant="caption" sx={{ fontWeight: 600 }}>
+                  {time}
+                </Typography>
+              </Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Box alignItems={"center"}>
+          <IconButton>
+            <Phone size={24} color="green" />
+          </IconButton>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default CallLogElement;
--- src/pages/dashboard/Call.tsx
+++ src/pages/dashboard/Call.tsx
@@ -1,7 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  Divider,
+  IconButton,
+  Link,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { MagnifyingGlass, Phone } from "phosphor-react";
+import { SimpleBarStyle } from "../../components/Scrollbar";
+import { CallList } from "../../data";
+import {
+  Search,
+  SearchIconWrapper,
+  SearchInputBase,
+} from "../../components/Search";
+import StartCall from "../../sections/main/StartCall";
+import CallLogElement from "../../components/CallLogElement";
 
 const Call = () => {
-  return <div>Call</div>;
+  const theme = useTheme();
+  const [openNewCallDialog, setOpenNewCallDialog] = useState(false);
+
+  const handleOpenNewCallDialog = () => {
+    setOpenNewCallDialog(true);
+  };
+
+  const handleCloseNewCallDialog = () => {
+    setOpenNewCallDialog(false);
+  };
+
+  return (
+    <>
+      <Stack direction={"row"} sx={{ width: "100%" }}>
+        {/* Left */}
+        <Box
+          sx={{
+            height: "100vh",
+            bgcolor: (theme) => theme.palette.background.paper,
+            width: 320,
+            boxShadow: "0 0 2px rgba(0,0,0,0.25)",
+          }}
+        >
+          <Stack
+            p={3}
+            spacing={2}
+            sx={{
+              maxHeight: "100vh",
+            }}
+          >
+            <Typography paddingLeft={1} variant="h5">
+              Call Log
+            </Typography>
+            <Stack>
+              <Search>
+                <SearchIconWrapper>
+                  <MagnifyingGlass color="#709CE6" />
+                </SearchIconWrapper>
+                <SearchInputBase placeholder="Search..." />
+              </Search>
+            </Stack>
+            <Stack spacing={1}>
+              <Stack
+                direction={"row"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                spacing={1.5}
+              >
+                <Typography
+                  variant="subtitle2"
+                  component={Link}
+                  sx={{ cursor: "pointer" }}
+                  onClick={handleOpenNewCallDialog}
+                >
+                  Start New Conversation
+                </Typography>
+                <IconButton onClick={handleOpenNewCallDialog}>
+                  <Phone
+                    size={24}
+                    style={{ color: theme.palette.primary.main }}
+                  />
+                </IconButton>
+              </Stack>
+              <Divider />
+            </Stack>
+            <Stack
+              spacing={2}
+              sx={{ flexGrow: 1, overflowY: "scroll", height: "100%" }}
+            >
+              <SimpleBarStyle timeout={500}>
+                <Stack spacing={2.4}>
+                  {CallList.map((chat) => (
+                    <CallLogElement key={chat.id} {...chat} />
+                  ))}
+                </Stack>
+              </SimpleBarStyle>
+            </Stack>
+          </Stack>
+        </Box>
+        {/* Right */}
+      </Stack>
+      {openNewCallDialog && (
+        <StartCall
+          open={openNewCallDialog}
+          handleClose={handleCloseNewCallDialog}
+        />
+      )}
+    </>
+  );
 };
 
 export default Call;
