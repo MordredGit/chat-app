4c28592ea65b973b5ab8a42e085327412bf5f520
MordredGit <mshagun2001@gmail.com>
2023-11-12 11:49:42 +0530

    Feat: Added logout functionality

---
+++ src/pages/auth/Logout.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "../../redux/store";
+import { LogOutUser } from "../../redux/slices/auth";
+import { Link, useNavigate } from "react-router-dom";
+
+const Logout = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // console.log("In logout");
+
+  useEffect(() => {
+    dispatch(LogOutUser())
+      .then(() => navigate("/auth/login"))
+      .catch((err: any) => console.log(err));
+  }, [dispatch, navigate]);
+  return (
+    <div>
+      Logged out <Link to={"/auth/login"}>Log in again</Link>
+    </div>
+  );
+};
+
+export default Logout;
